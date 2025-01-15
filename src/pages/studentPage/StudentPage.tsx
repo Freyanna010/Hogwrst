c8347c15ad7e84c54d889f25a411c4b4efd51836
Ann <socevilaf@gmail.com>
2025-01-15 13:43:35 +0300

    fix

--- src/pages/studentPage/StudentPage.tsx
+++ src/pages/studentPage/StudentPage.tsx
@@ -1,15 +1,13 @@
 import StudentInfoCard from "@/componets/StudentInfoCard";
-import { fetchStudentDataById } from "@/features/studentsThunks";
 import { AppDispatch, RootState } from "@/store/store";
 import  { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
 
 const StudentPage = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const { id } = useParams();
+
   const { currentStudent } = useSelector((state: RootState) => state.students);
-// TODO: добавать переход на нового студента  
+
   useEffect(() => {
     // if (id) {
     //   dispatch(fetchStudentDataById(id));
